dee3770f061bda47ea7325be1dfb0342a2824f41
beatriz <beatrizffeo@gmail.com>
2024-04-30 16:34:33 -0600

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES 
+('Mr. Chocolate', 1, 'M', 'Dark Brown', 'Agressive', True),
+('Rowdy', 5, 'M', 'Gray', 'Nice', False),
+('Tabitha', 3, 'F', 'Black', 'Hungry', True),
+('Sergeant Brown', 7, 'M', 'Brown', 'Happy', True),
+('Melissa', 2, 'F', 'White', 'Enegertic', False),
+('Grinch', 10, 'M', 'Green', 'Evil', True),
+('Wendy', 4, 'F', 'Pink', 'Strong', True),
+(NULL, 6, 'M', 'Orange', 'Weird', True);
